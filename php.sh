1e04ad8a394479a9f3ef29ab36e8dda6447a2a08
IKokiri <developer.luizmendes@gmail.com>
2020-01-15 11:47:01 -0300

    adição do statup

--- php.sh
+++ php.sh
@@ -3,4 +3,3 @@ sudo apt -y install software-properties-common
 sudo add-apt-repository ppa:ondrej/php
 sudo apt-get update
 sudo apt -y install php7.4
-php -v
---
+++ startup.sh
@@ -0,0 +1,6 @@
+chmod +x php.sh
+chmod +x composer.sh
+./php.sh
+./composer.sh
+php -v
+composer -v
