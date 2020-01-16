64deb3c7160b260a2c9c97c739dd0fcd818ec0dd
IKokiri <developer.luizmendes@gmail.com>
2020-01-16 09:32:52 -0300

    adição de exts

--- php.sh
+++ php.sh
@@ -3,3 +3,6 @@ sudo apt -y install software-properties-common
 sudo add-apt-repository ppa:ondrej/php
 sudo apt-get update
 sudo apt -y install php7.4
+sudo apt-get -y install php-zip
+sudo apt-get install php-mbstring
+sudo apt-get install php-xml
