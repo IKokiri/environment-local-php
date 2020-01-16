ad481996ea9597f03365262bef53cf97f0d8d050
IKokiri <developer.luizmendes@gmail.com>
2020-01-16 17:48:09 -0300

    remoção do sudo no inicio

--- php.sh
+++ php.sh
@@ -1,8 +1,8 @@
-sudo apt-get update
-sudo apt -y install software-properties-common
-sudo add-apt-repository ppa:ondrej/php
-sudo apt-get update
-sudo apt -y install php7.4
-sudo apt-get -y install php-zip
-sudo apt-get install php-mbstring
-sudo apt-get install php-xml
+apt-get update
+apt -y install software-properties-common
+add-apt-repository ppa:ondrej/php
+apt-get update
+apt -y install php7.4
+apt-get -y install php-zip
+apt-get install php-mbstring
+apt-get install php-xml
